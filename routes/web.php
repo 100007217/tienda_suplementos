4f63e778f910874ea7dda2a268c1855eb04ddab2
Dani <100007160.joan23@fje.edu>
2022-02-21 15:13:31 +0100

    commmmit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\UsuarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/inicio', [ProductoController::class, 'inicio']);
+
+Route::get('/admin/crear', [ProductoController::class, 'crar']);
